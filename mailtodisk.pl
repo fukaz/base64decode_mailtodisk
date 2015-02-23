37b236a7fd8fa8e795761a3df85f6fe6d03395c7
fkz <fkz222@yahoo.co.jp>
2015-02-23 22:19:48 +0900

    BASE64エンコードされていない場合にも対応

--- mailtodisk.pl
+++ mailtodisk.pl
@@ -9,6 +9,16 @@ use MIME::Base64;
 use POSIX 'strftime';
 use Time::HiRes;
 
+my $debug = 0;
+foreach my $arg (@ARGV) {
+    if ($arg eq '-d') {
+	$debug = 1;
+    }
+}
+if ($debug) {
+    use Cwd;
+}
+
 #my $time = time();
 #print "$time\n";
 my $timems = Time::HiRes::time;
@@ -21,23 +31,64 @@ print "\n";
 #my $ms = $timems * 1000000 - int($timems) * 1000000;
 #print "$ms\n";
 
-my $file = 'c:/xampp/mailoutput/mail-' . strftime("%Y%m%d-%H%M%S-", localtime($time[0])) . $time[1] . ".txt";
-my $rawfile = 'c:/xampp/mailoutput/mail-' . strftime("%Y%m%d-%H%M%S-", localtime($time[0])) . $time[1] . "raw.txt";
+my $file = 'mailoutput/mail-' . strftime("%Y%m%d-%H%M%S-", localtime($time[0])) . $time[1] . ".txt";
+my $rawfile = 'mailoutput/mail-' . strftime("%Y%m%d-%H%M%S-", localtime($time[0])) . $time[1] . "raw.txt";
 print "$file\n";
+if ($debug) {
+    my $logfile = 'c:/xampp/mailoutput/mailtodisk.log';
+    if (open(LOG, '>>', $logfile)) {
+	my $wd = Cwd::getcwd();
+	print LOG "$wd->$file\n";
+    } else {
+	die "file write open error: $file";
+    }
+}
 if (open(MAIL, '>', $rawfile) && open(DECODEMAIL, '>', $file)) {
     my $body = '';
-    my $type;
+    my $type = '';
     my $value;
+    my $orgcode = '';
+    my $base64flg = 0;
     while (<STDIN>) {
 	print MAIL "$_";
-	if (/^$/) {
+	chomp;
+	if ($type eq 'body') {
+	    #if ($debug) {
+	    #    print LOG "$_";
+	    #}
+	    $body .= $_ . "\n";
+	} elsif (/^$/) {
 	    $type = 'body';
-	} elsif (/:/) {
+	} elsif (/\s*:\s*/) {
 	    $type = $`;
 	    $value = $';
 	    my $line = '';
 	    do {
-		if ($value =~ /=\?ISO-2022-JP\?B\?(.*?)\?=/) {
+		if ($type =~ /^Content-Transfer-Encoding$/i) {
+		    if ($value =~ /^base64$/i) {
+			$base64flg = 1;
+		    }
+		    $line .= $value;
+		    $value = '';
+		    #if ($debug) {
+		    #	print LOG "$type: $line";
+		    #}
+		} elsif ($type =~ /^Content-Type$/i) {
+		    if ($value =~ /charset="?([\w\-]+)"?/i) {
+			$orgcode = $1;
+			#if ($debug) {
+			#    print LOG "orgcode=$orgcode\n";
+			#}
+			if ($orgcode =~ /^ISO-2022-JP$/i) {
+			    $value =~ s/$orgcode/shift_jis/;
+			}
+		    }
+		    $line .= $value;
+		    $value = '';
+		    #if ($debug) {
+		    #	print LOG "$type: $line";
+		    #}
+		} elsif ($value =~ /=\?ISO-2022-JP\?B\?(.*?)\?=/i) {
 		    $line .= $`;
 		    $value = $';
 		    my $str = decode_base64($1);
@@ -47,18 +98,39 @@ if (open(MAIL, '>', $rawfile) && open(DECODEMAIL, '>', $file)) {
 		} else {
 		    $line .= $value;
 		    $value = '';
+		    if ($debug) {
+		        print LOG "$type: $line\n";
+		    }
 		}
 	    } until ($value eq '');
-	    print DECODEMAIL $type . ':' . $line;
-	} elsif ($type eq 'body') {
-	    $body .= $_;
+	    if ($type) {
+		print DECODEMAIL $type . ': ' . $line . "\n";
+		#if ($debug) {
+		#    print LOG "line=$type: $line";
+		#}
+	    }
 	}
     }
-    my $str = decode_base64($body);
-    my $decoded = decode('JIS', $str);
-    my $encoded = encode('Shift_JIS', $decoded);
-    print DECODEMAIL "\n$encoded\n";
+    my $str;
+    if ($base64flg) {
+	$str = decode_base64($body);
+    } else {
+	$str = $body;
+    }
+    my $decoded;
+    my $encoded;
+    if ($orgcode =~ /^ISO-2022-JP$/i) {
+	$decoded = decode('JIS', $str);
+	$encoded = encode('Shift_JIS', $decoded);
+    } else {
+	$encoded = $str;
+    }
+    print DECODEMAIL "\n$encoded";
     close(DECODEMAIL);
+    close(MAIL);
+    if ($debug) {
+	close(LOG);
+    }
 } else {
     die "file write open error: $file";
 }
